5cd58d86fbe651a37eb5c00a5da1f79d653a8e03
kifff <130282724+kifff@users.noreply.github.com>
2024-07-04 02:59:05 +0700

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -43,7 +43,7 @@
 
 
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "node_modules/bootstrap/dist/css/bootstrap.min.css";
 import portfolioImage from "../assets/images/kifffwebta.png";
 import "./home.css";
 
