38a1d9e9d2be87c88ee66cf108b6173c04e9f4b4
kifff <130282724+kifff@users.noreply.github.com>
2024-07-04 03:18:42 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,7 +14,7 @@
 
 
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const Footer = () => {
   return (
