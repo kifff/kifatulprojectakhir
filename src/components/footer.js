0cc3bf731dfa1ee4708283386e1310eec595de59
kifff <130282724+kifff@users.noreply.github.com>
2024-07-04 01:59:48 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,28 @@
+// import React from "react";
+// import "./footer.css";
+
+// const Footer = () => {
+//   return (
+//     <footer className="footer">
+//       <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+
+
 import React from "react";
-import "./footer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>&copy; 2023 My Portfolio. All rights reserved.</p>
+    <footer className="footer bg-dark text-white text-center py-2 fixed-bottom">
+      <p className="mb-0">
+        {" "}
+        Create By kifff &copy; 2023 My Portfolio. All rights reserved.
+      </p>
     </footer>
   );
 };
