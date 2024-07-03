fe44b4f97a9eb50c6f91ea34f2a385565f3da226
kifff <130282724+kifff@users.noreply.github.com>
2024-07-04 02:58:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from "./pages/about";
 import Projects from "./pages/project";
 import "./App.css";
 import Contact from "./pages/contact";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
