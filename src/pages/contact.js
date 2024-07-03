db5357570f4a50264afa93e9ea7247c9ff18e2e1
kifff <130282724+kifff@users.noreply.github.com>
2024-07-04 01:56:06 +0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,82 @@
+// import React from "react";
+// import "./contact.css";
+
+// const Contact = () => {
+//   return (
+//     <section id="contact" className="contact">
+//       <div className="desc">
+//         <h2>Contact</h2>
+//         <form>
+//           <label htmlFor="name">Name:</label>
+//           <input type="text" id="name" name="name" required />
+
+//           <label htmlFor="email">Email:</label>
+//           <input type="email" id="email" name="email" required />
+
+//           <label htmlFor="message">Message:</label>
+//           <textarea id="message" name="message" required></textarea>
+
+//           <button type="submit">Send</button>
+//         </form>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Contact;
+
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./contact.css";
 
 const Contact = () => {
   return (
-    <section id="contact" className="contact">
-      <div className="desc">
-        <h2>Contact</h2>
-        <form>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
+    <div className="bg">
+      <div id="contact" className="container my-5 col-3">
+        <div className="desc">
+          <h2 className="text-center mb-4 text-white ">Contact</h2>
+          <form className="text-white">
+            <div className="form-group ">
+              <label htmlFor="name ">Name:</label>
+              <input
+                type="text"
+                className="form-control "
+                id="name"
+                name="name"
+                required
+              />
+            </div>
 
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+            <div className="form-group ">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                required
+              />
+            </div>
 
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" required></textarea>
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea
+                className="form-control"
+                id="message"
+                name="message"
+                required
+              ></textarea>
+            </div>
 
-          <button type="submit">Send</button>
-        </form>
+            <button type="submit" className="btn btn-dark mt-3">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
 export default Contact;
+
